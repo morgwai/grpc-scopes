4f5eecb6de9121c5ac5960468248db1a6fd37017
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2021-08-25 20:19:00 +0700

    sample: RecordStorageServer: cleanup

--- sample/src/main/java/pl/morgwai/samples/grpc/scopes/grpc/RecordStorageServer.java
+++ sample/src/main/java/pl/morgwai/samples/grpc/scopes/grpc/RecordStorageServer.java
@@ -1,7 +1,6 @@
 // Copyright (c) Piotr Morgwai Kotarbinski, Licensed under the Apache License, Version 2.0
 package pl.morgwai.samples.grpc.scopes.grpc;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
@@ -12,7 +11,6 @@ import javax.persistence.Persistence;
 import com.google.inject.Guice;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
-
 import io.grpc.Server;
 import io.grpc.ServerInterceptors;
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
@@ -28,32 +26,34 @@ public class RecordStorageServer {
 
 
 
-	EntityManagerFactory entityManagerFactory;
-	ContextTrackingExecutor jpaExecutor;
-	static final String PERSISTENCE_UNIT_NAME = "RecordDb";
-	static final int JDBC_CONNECTION_POOL_SIZE = 3;  // same as in persistence.xml
-
-	final int port;
 	static final String PORT_ENV_NAME = "PORT";
 	public static final int DEFAULT_PORT = 6666;
 
-	final int maxConnectionIdleSeconds;
 	static final String MAX_CONNECTION_IDLE_ENV_NAME = "MAX_CONNECTION_IDLE_SECONDS";
 	static final int DEFAULT_MAX_CONNECTION_IDLE = 60;
 
-	final int maxConnectionAgeMinutes;
 	static final String MAX_CONNECTION_AGE_ENV_NAME = "MAX_CONNECTION_AGE_MINUTES";
 	static final int DEFAULT_MAX_CONNECTION_AGE = 5;
 
-	final int maxConnectionAgeGraceHours;
 	static final String MAX_CONNECTION_AGE_GRACE_ENV_NAME = "MAX_CONNECTION_AGE_GRACE_HOURS";
 	static final int DEFAULT_MAX_CONNECTION_AGE_GRACE = 24;
 
-	Server recordStorageServer;
+
+
+	final Server recordStorageServer;
+
+	final EntityManagerFactory entityManagerFactory;
+	final ContextTrackingExecutor jpaExecutor;
+	static final String PERSISTENCE_UNIT_NAME = "RecordDb";  // same as in persistence.xml
+	static final int JDBC_CONNECTION_POOL_SIZE = 3;  // same as in persistence.xml
 
 
 
-	void startAndAwaitTermination() throws IOException, InterruptedException {
+	RecordStorageServer(
+			int port,
+			int maxConnectionIdleSeconds,
+			int maxConnectionAgeMinutes,
+			int maxConnectionAgeGraceHours) throws Exception {
 		final var grpcModule = new GrpcModule();
 
 		entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
@@ -81,71 +81,43 @@ public class RecordStorageServer {
 			.maxConnectionAgeGrace(maxConnectionAgeGraceHours, TimeUnit.HOURS)
 			.addService(ServerInterceptors.intercept(service, grpcModule.contextInterceptor))
 			.build();
-
-		Runtime.getRuntime().addShutdownHook(shutdownHook);
 		recordStorageServer.start();
 		log.info("server started on port " + port);
-		recordStorageServer.awaitTermination();
+
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			try {
+				recordStorageServer.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {}
+			if (recordStorageServer.isTerminated()) {
+				log.info("gRPC server shutdown completed");
+			} else {
+				log.info("gRPC server has NOT shutdown cleanly");
+			}
+			jpaExecutor.tryShutdownGracefully(5);
+			entityManagerFactory.close();
+			log.info("entity manager factory shutdown completed");
+			((LogManager) LogManager.getLogManager()).manualReset();
+		}));
 	}
-
-
-
-	Thread shutdownHook = new Thread(() -> {
-		try {
-			recordStorageServer.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {}
-		if (recordStorageServer.isTerminated()) {
-			log.info("gRPC server shutdown completed");
-		} else {
-			log.info("gRPC server has NOT shutdown cleanly");
-		}
-		jpaExecutor.tryShutdownGracefully(5);
-		entityManagerFactory.close();
-		log.info("entity manager factory shutdown completed");
-		((LogManager) LogManager.getLogManager()).manualReset();
-	});
 
 
 
 	public static void main(String args[]) throws Exception {
-		int port = getIntFromEnv(PORT_ENV_NAME, DEFAULT_PORT);
-		int maxConnectionIdleSeconds =
-				getIntFromEnv(MAX_CONNECTION_IDLE_ENV_NAME, DEFAULT_MAX_CONNECTION_IDLE);
-		int maxConnectionAgeMinutes =
-				getIntFromEnv(MAX_CONNECTION_AGE_ENV_NAME, DEFAULT_MAX_CONNECTION_AGE);
-		int maxConnectionAgeGraceHours =
-				getIntFromEnv(MAX_CONNECTION_AGE_GRACE_ENV_NAME, DEFAULT_MAX_CONNECTION_AGE_GRACE);
-
 		new RecordStorageServer(
-			port,
-			maxConnectionIdleSeconds,
-			maxConnectionAgeMinutes,
-			maxConnectionAgeGraceHours
-		).startAndAwaitTermination();
+			getIntFromEnv(PORT_ENV_NAME, DEFAULT_PORT),
+			getIntFromEnv(MAX_CONNECTION_IDLE_ENV_NAME, DEFAULT_MAX_CONNECTION_IDLE),
+			getIntFromEnv(MAX_CONNECTION_AGE_ENV_NAME, DEFAULT_MAX_CONNECTION_AGE),
+			getIntFromEnv(MAX_CONNECTION_AGE_GRACE_ENV_NAME, DEFAULT_MAX_CONNECTION_AGE_GRACE)
+		).recordStorageServer.awaitTermination();
 	}
 
 	static int getIntFromEnv(String envVarName, int defaultValue) {
-		int value = defaultValue;
-		try {
-			value = Integer.parseInt(System.getenv(envVarName));
-			log.info(envVarName + '=' + value);
-		} catch (Exception e) {
-			log.info(envVarName + " unset or invalid, using default " + defaultValue);
+		String value = System.getenv(envVarName);
+		if (value == null) {
+			log.info(envVarName + " unset, using default " + defaultValue);
+			return defaultValue;
 		}
-		return value;
-	}
-
-
-
-	RecordStorageServer(
-			int port,
-			int maxConnectionIdleSeconds,
-			int maxConnectionAgeMinutes,
-			int maxConnectionAgeGraceHours) {
-		this.port = port;
-		this.maxConnectionIdleSeconds = maxConnectionIdleSeconds;
-		this.maxConnectionAgeMinutes = maxConnectionAgeMinutes;
-		this.maxConnectionAgeGraceHours = maxConnectionAgeGraceHours;
+		return Integer.parseInt(value);
 	}
 
 
