a5e6026469d2ecbd38dff5e47afb7049e8b1f90d
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2021-10-18 01:39:03 +0700

    auto-generate build.gradle from pom.xml

---
+++ sample/generate-build.gradle.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+rm -f build.gradle settings.gradle &&
+./gradlew init --type pom --dsl groovy &&
+sed -n -e '/^dependencies {/,/^}/p' <build.gradle |head -n -1 >dependencies.txt &&
+egrep 'compileOnly|providedCompile' <dependencies.txt |sed -e 's#compileOnly#testImplementation#' \
+    -e 's#providedCompile#testImplementation#' >testDependencies.txt &&
+echo '// Generated from build.gradle.header and pom.xml using generate-build.gradle.sh' \
+    >build.gradle &&
+cat build.gradle.header dependencies.txt testDependencies.txt >>build.gradle &&
+echo -e '}\n' >>build.gradle &&
+rm dependencies.txt testDependencies.txt &&
+
+echo -n "group = '" >>build.gradle &&
+mvn -q --non-recursive exec:exec -Dexec.executable=echo '-Dexec.args=-n ${project.groupId}' \
+    >>build.gradle &&
+echo "'" >>build.gradle &&
+echo -n "version = '" >>build.gradle &&
+mvn -q --non-recursive exec:exec -Dexec.executable=echo '-Dexec.args=-n ${project.version}' \
+    >>build.gradle &&
+echo "'" >>build.gradle &&
+echo -n "def grpcVersion = '" >>build.gradle &&
+mvn -q --non-recursive exec:exec -Dexec.executable=echo '-Dexec.args=-n ${grpc.version}' \
+    >>build.gradle &&
+echo "'" >>build.gradle &&
+echo -n "def protobufVersion = '" >>build.gradle &&
+mvn -q --non-recursive exec:exec -Dexec.executable=echo '-Dexec.args=-n ${protobuf.version}' \
+    >>build.gradle &&
+echo "'" >>build.gradle &&
+
+cat build.gradle.protobuf >>build.gradle
