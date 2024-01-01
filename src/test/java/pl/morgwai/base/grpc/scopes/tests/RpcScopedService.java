5e91aeff7dbad3c486032eebfd1c996cba0acdd9
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2024-01-02 00:21:31 +0100

    IntegrationTests: ensure hashes of injected objects never repeat

--- src/test/java/pl/morgwai/base/grpc/scopes/tests/RpcScopedService.java
+++ src/test/java/pl/morgwai/base/grpc/scopes/tests/RpcScopedService.java
@@ -1,4 +1,17 @@
 // Copyright (c) Piotr Morgwai Kotarbinski, Licensed under the Apache License, Version 2.0
 package pl.morgwai.base.grpc.scopes.tests;
 
-public class RpcScopedService {}
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+
+public class RpcScopedService {
+
+	static final AtomicInteger idSequence = new AtomicInteger(0);
+	final int id = idSequence.incrementAndGet();
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+}
